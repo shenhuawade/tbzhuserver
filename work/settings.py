2b02a29eb4e17a2ad73d3e35ce614baa6302ac95
shenhuawade <317909531@qq.com>
2019-01-11 00:03:00 +0800

    Signed-off-by: shenhuawade <317909531@qq.com>

--- work/settings.py
+++ work/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fp5$-jgkka^o32djlyhi3ue0&ium*h!kmaxvelg==k+uo*mm3@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # ALLOWED_HOSTS = ['*']
